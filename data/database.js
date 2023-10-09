2e99e40debb5488fcaec27d48182a8de03540973
GyaneshwerJha <98211593+GyaneshwerJha@users.noreply.github.com>
2023-10-09 18:35:33 +0530

    fixed engines

--- data/database.js
+++ data/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connect = () => mongoose
     .connect(process.env.MONGO_URL)
-    .then(() => console.log("Database connected to the server"))
+    .then((c) => console.log(`Database connected with ${c.connection.host}`))
     .catch((e) => console.log(e));
 
 export { connect }
